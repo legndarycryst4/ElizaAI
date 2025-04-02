9b7852d9a46304ab080cc34d8cf397bcde99fc62
Amberlee <107285224+legndarycryst4@users.noreply.github.com>
2025-04-02 18:54:49 -0400

    Update twitchbot.py

--- src/twitchbot.py
+++ src/twitchbot.py
@@ -2,9 +2,9 @@ from typing import List
 
 from twitchio.ext import commands  # type: ignore
 
-from .chat import gpt3_completion
+from .chat import gpt3_completion  # You may need to update this function to use Llama instead of OpenAI
 from .chattypes import ChatCompletionMessage
-from .credentials import BOT_NAME, TWITCH_CHANNEL, TWITCH_TOKEN
+from .credentials import BOT_NAME, TWITCH_CHANNEL, TWITCH_TOKEN, LLAMA_MODEL_PATH
 from .filter_message import check_and_filter_user_message
 from .texttospeech_evenlabs import get_speech_by_text
 from .utils import open_file
@@ -17,9 +17,6 @@ class Bot(commands.Bot):
     conversation: List[ChatCompletionMessage] = list()
 
     def __init__(self, speaker_bot=False, speaker_alias="Default"):
-        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...  # noqa: E501 B950
-        # prefix can be a callable, which returns a list of strings or a string...  # noqa: E501
-        # initial_channels can also be a callable which returns a list of strings...  # noqa: E501
         Bot.conversation.append(
             {"role": "system", "content": open_file("prompt_chat.txt")}
         )
@@ -32,8 +29,6 @@ class Bot(commands.Bot):
         )
 
     async def event_ready(self):
-        # Notify us when everything is ready!
-        # We are logged in and ready to chat and use commands...
         print(f"Logged in as | {self.nick}")
 
     async def event_message(self, message):
@@ -49,7 +44,8 @@ class Bot(commands.Bot):
 
         Bot.conversation.append({"role": "user", "content": user_question})
 
-        bot_response = gpt3_completion(Bot.conversation)
+        # Update this line to call Llama instead of OpenAI
+        bot_response = gpt3_completion(Bot.conversation, model_path=LLAMA_MODEL_PATH)  
         print(f"{BOT_NAME}:", bot_response)
 
         conversation = {"role": "assistant", "content": bot_response}
@@ -59,38 +55,18 @@ class Bot(commands.Bot):
         if len(Bot.conversation) > CONVERSATION_LIMIT:
             Bot.conversation = Bot.conversation[1:]
 
-        if (
-            self.speaker_bot
-        ):  # if speakerbot flag is set, skip everything else, just send
-            # message to sb
+        if self.speaker_bot:
             self.send_to_speaker_bot(bot_response)
             await self.handle_commands(message)
             return
 
         get_speech_by_text(user_question, bot_response)
 
-        # Print the contents of our message to console...
-
-        # Since we have commands and are overriding the default `event_message`
-        # We must let the bot know we want to handle and invoke our commands...
         await self.handle_commands(message)
 
     @commands.command(name="hola", aliases=["op", "haupei", "alo", "buen día"])
     async def hello(self, ctx: commands.Context):
-        # Here we have a command hello, we can invoke our command with our
-        # prefix and command name e.g ?hello
-        # We can also give our commands aliases (different names)
-        # to invoke with.
-
-        # Send a hello back!
-        # Sending a reply back to the channel is easy... Below is an example.
         await ctx.send(f"Hello {ctx.author.name}!")
 
     async def send_to_speaker_bot(self, message: str, verbose=False) -> None:
-        """
-        Sends message to speaker.bot websocket using standart setup
-        @param message: the message you want to have spoken
-        @param verbose: set to true if debugging info is wanted
-        """
-
         await open_websocket(self.speaker_alias, message, verbose)
